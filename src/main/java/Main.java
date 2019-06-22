0a2c779393aba89c1e3aefc1c2ff242babb27bdd
Flipper (Thomas) <thomasviolent@gmail.com>
2019-06-22 11:07:20 +0200

    fix maven + passage sur log4J

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,55 +1,60 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.profesorfalken.jsensors.JSensors;
-import com.profesorfalken.jsensors.model.components.Components;
-import com.profesorfalken.jsensors.model.components.Cpu;
-import com.profesorfalken.jsensors.model.sensors.Temperature;
 import org.apache.commons.io.IOUtils;
+import org.apache.logging.log4j.Level;
 import oshi.SystemInfo;
 import oshi.hardware.*;
 import oshi.software.os.OperatingSystem;
 
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.ObjectInputFilter;
 import java.io.PrintWriter;
 import java.sql.*;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
 
 public class Main {
 
-    public static final String PathConfigFile = "config.json";
+    //chemin fichier config
+    public static final String pathConfigFile = "config.json";
 
-    public Logger logger = Logger.getLogger("logger");
+    //objet de log java
+    public Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
         new Main();
     }
 
+    //classe principale
     public Main() {
         logger.log(Level.INFO, "Starting");
         ConfigOBJ config = readConfigFile();
+        logger.log(Level.INFO, "trying to connect to database");
         try {
             Connection conn = DriverManager.getConnection("jdbc:" + config.BDDAdress + "/" + config.BDDDatabase + "?useLegacyDatetimeCode=false&serverTimezone=Europe/Paris", config.BDDUsername, config.BDDPassword);
             Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery("select * from cpu_day" );
+            ResultSet rs = st.executeQuery("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA='" + config.BDDDatabase + "'" );
             ResultSetMetaData rsmd = rs.getMetaData();
-            rs.next();
 
-            System.out.println(rsmd.getColumnCount());
+            ArrayList<String> db = new ArrayList<>();
 
-            for (int i = 1; i <= rsmd.getColumnCount(); i++) {
-                System.out.println(rs.getString(i));
+            while (rs.next()) {
+                db.add(rs.getString(1));
             }
-            System.out.println();
+
+            logger.debug("Found the following table(s):");
+            for (String s : db) {
+                logger.debug(" - " + s);
+            }
+
             conn.close();
         } catch (Exception e) {
-            logger.log(Level.SEVERE, "Failed to connect to database");
-            logger.log(Level.SEVERE, e.toString());
+            logger.fatal("Failed to connect to database");
+            logger.fatal(e.toString());
             e.printStackTrace();
         }
 
@@ -129,8 +134,8 @@ public class Main {
 
     public void genConfigFile() {
         ConfigOBJ config = new ConfigOBJ();
-        config.BDDAdress = "localhost";
-        config.BDDDatabase = "systemMonitoring";
+        config.BDDAdress = "mysql://localhost";
+        config.BDDDatabase = "system_monitoring";
         config.BDDUsername = "root";
         config.BDDPassword = "";
 
@@ -138,26 +143,37 @@ public class Main {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String json = gson.toJson(config);
         try {
-            PrintWriter out = new PrintWriter(PathConfigFile);
+            PrintWriter out = new PrintWriter(pathConfigFile);
             out.println(json);
             out.close();
+            logger.log(Level.INFO, "basic config file successfully created, please edit it and restart the software");
+            System.exit(0);
         } catch(Exception e) {
-            logger.log(Level.SEVERE, "failed to write config file");
-            logger.log(Level.SEVERE, e.getMessage());
+            logger.fatal("failed to write config file");
+            logger.fatal(e.getMessage());
+            System.exit(-1);
         }
     }
 
     public ConfigOBJ readConfigFile() {
         Gson gson = new Gson();
 
-        try(FileInputStream inputStream = new FileInputStream(PathConfigFile)) {
-            String everything = IOUtils.toString(inputStream, "UTF-8");
-            ConfigOBJ obj = gson.fromJson(everything, ConfigOBJ.class);
-            return obj;
-        } catch (Exception e) {
-            logger.log(Level.SEVERE, "failed to read config file");
-            logger.log(Level.SEVERE, e.getMessage());
+        File f = new File(pathConfigFile);
+        if(f.exists() && !f.isDirectory()) {
+            try(FileInputStream inputStream = new FileInputStream(pathConfigFile)) {
+                String everything = IOUtils.toString(inputStream, "UTF-8");
+                ConfigOBJ obj = gson.fromJson(everything, ConfigOBJ.class);
+                return obj;
+            } catch (Exception e) {
+                logger.fatal("failed to read config file");
+                logger.fatal(e.getMessage());
+                System.exit(-1);
+                return null;
+            }
+        } else {
+            logger.warn("Config file not found, tyring to create it");
+            genConfigFile();
         }
-        return null;
+        return null; //ne devrais jamais être atteind
     }
 }
